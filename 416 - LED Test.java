a68ac8b37b1ff1c63e56b0efe308cb1ad4375fbe
VadimGladchenko <vadimgladchenko@mail.ru>
2016-11-17 13:30:20 +0200

    Update file

--- 416 - LED Test.java
+++ 416 - LED Test.java
@@ -33,38 +33,33 @@ class Main
     }
 
     void Begin() {
-        String[] indicators;
-        String digitalSymbols[] = {
-                "YYYYYYN",  //0
-                "NYYNNNN",  //1
-                "YYNYYNY",  //2
-                "YYYYNNY",  //3
-                "NYYNNYY",  //4
-                "YNYYNYY",  //5
-                "YNYYYYY",  //6
-                "YYYNNNN",  //7
-                "YYYYYYY",  //8
-                "YYYYNYY"   //9
-        };
+        int digitalSymbols[] = { 0x7E, 0x30, 0x6D, 0x79, 0x33, 0x5B, 0x5F, 0x70, 0x7F, 0x7B };
+        int indicators[];
 
         int inputCount;
         String input;
         StringTokenizer idata;
 
-        input = ReadLn ();
-        idata = new StringTokenizer (input);
-        inputCount = Integer.parseInt (idata.nextToken());
+        input = ReadLn();
+        idata = new StringTokenizer(input);
+        inputCount = Integer.parseInt(idata.nextToken());
 
-        while(inputCount  != 0)
+        while(inputCount != 0)
         {
-            indicators = new String[inputCount];
             boolean dataIsCountDown = false;
+            indicators = new int[inputCount];
 
-            for (int i = 0; i < inputCount; i++)
-                indicators[i] = ReadLn();
+            for (int i = 0; i < inputCount; i++) {
+                input = ReadLn();
+                for (int j = 0; j < 7; j++)
+                {
+                    if(input.charAt(j) == 'Y') indicators[i] |= (1) << (6-j);
+                    else indicators[i] |= (0) << (6-j);
+                }
+            }
 
             for (int j = 9; j >= 0; j--) {
-                if(isCountDown(indicators, digitalSymbols, j, 0))
+                if(isCountDown(indicators, digitalSymbols, j, 0, 0))
                 {
                     dataIsCountDown = true;
                     break;
@@ -76,21 +71,22 @@ class Main
             else
                 System.out.println("MISMATCH");
 
-            input = ReadLn ();
-            idata = new StringTokenizer (input);
-            inputCount = Integer.parseInt (idata.nextToken());
+            input = ReadLn();
+            idata = new StringTokenizer(input);
+            inputCount = Integer.parseInt(idata.nextToken());
         }
     }
 
-    boolean isCountDown(String[] indicators, String[] digitalSymbols, int testNumber, int index)
+    boolean isCountDown(int[] indicators, int[] digitalSymbols, int testNumber, int index, int badMask)
     {
-        for (int i = 0; i < 7; i++) {
-            if (indicators[index].charAt(i) == 'Y' && digitalSymbols[testNumber].charAt(i) != 'Y') return false;
-        }
+        if((indicators[index] & (127 - digitalSymbols[testNumber])) != 0) return false;     //implication s->s* = (s* & !s)
+        if((indicators[index] & badMask) != 0) return false;
+
+        badMask = indicators[index] ^ digitalSymbols[testNumber];
 
         if(index + 1 < indicators.length) {
             if (testNumber - 1 < 0) return false;
-            if (!isCountDown(indicators, digitalSymbols, testNumber - 1, index + 1)) return false;
+            if (!isCountDown(indicators, digitalSymbols, testNumber - 1, index + 1, badMask)) return false;
         }
 
         return true;
